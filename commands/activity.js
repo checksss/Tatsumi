f9f370d630ebbb7653fb1f82e4f2b43b95c88571
ItzLightyHD <itzlightyhd@gmail.com>
2020-04-15 12:56:27 +0200

    Update activity.js

--- commands/activity.js
+++ commands/activity.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args, ops) => {
+exports.run = async (client, message, args, ops) => {
 	let activity = await (args.join(' '), function(err, res) {
 		client.user.setActivity(`${args[0]}`);
 		message.channel.send(`Successfully set status to: **${args[0]}**`);
