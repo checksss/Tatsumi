f5b781d897708197af11b42514266060112e645b
ItzLightyHD <itzlightyhd@gmail.com>
2020-04-15 13:01:55 +0200

    Final one, hoping that will fix all

--- commands/activity.js
+++ commands/activity.js
@@ -1,7 +1,7 @@
-exports.run = async (client, message, args, ops) => {
-	let activity = await (args.join(' '), function(err, res) {
+exports.run = (client, message, args, ops) => {
+	args.join(' '), function(err, res) {
 		client.user.setActivity(`${args[0]}`);
 		message.channel.send(`Successfully set status to: **${args[0]}**`);
 		console.log(`Successfully set status to: ${args[0]}`);
-	});
+	}
 }
