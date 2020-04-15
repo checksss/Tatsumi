332d910456fee413260769487d6e49d85681fb87
ItzLightyHD <itzlightyhd@gmail.com>
2020-04-15 16:09:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 const shard = new ShardingManager('./tamarro.js', {
-  token: "Njk4MDg1MzQ1MDg2MDEzNDgw.XpBz8w.p4g4qhJYURtlaB49d6CquSN-3r0",
+  token: process.env.TOKEN,
   autoSpawn: true
 });
 
