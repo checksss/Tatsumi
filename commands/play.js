2423eedfaaeefd0af6a9bd7bab4d7c1a73f7491e
ItzLightyHD <itzlightyhd@gmail.com>
2020-05-03 22:36:09 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -6,7 +6,8 @@ exports.run = async (client, message, args, ops) => {
 
     if (!args[0]) return message.reply("Please search or input an url following the command!");
 
-    let validate = await ytdl.validateURL(args[0]).catch (err) {};
+    let validate = await ytdl.validateURL(args[0]).catch(e){
+    };
 
     if (!validate) {
       let commandFile = require("./search.js");
