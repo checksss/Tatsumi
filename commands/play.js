2e8c289d2196ca5b39201ae5a28cb1f1c187f0d4
ItzLightyHD <itzlightyhd@gmail.com>
2020-05-03 22:22:41 +0200

    Not showing validate error...

--- commands/play.js
+++ commands/play.js
@@ -6,7 +6,10 @@ exports.run = async (client, message, args, ops) => {
 
     if (!args[0]) return message.reply("Please search or input an url following the command!");
 
-    let validate = await ytdl.validateURL(args[0]);
+    let validate = await try {
+    	ytdl.validateURL(args[0]);
+    } catch (error) {
+    }
 
     if (!validate) {
       let commandFile = require("./search.js");
