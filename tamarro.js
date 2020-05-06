93ccff9df5d8d754a40ff753e675bad5093e596b
ItzLightyHD <itzlightyhd@gmail.com>
2020-05-06 11:07:46 +0200

    Update tamarro.js

--- tamarro.js
+++ tamarro.js
@@ -5,7 +5,6 @@ pingFrequency = (30 * 1000);
 
 const prefix = "%";
 const ownerID = '480987124405895168';
-const joinChannel = client.channels.cache.get("702228736988413954");
 
 client.on("guildCreate", guild => {
     console.log("Joined a new guild: " + guild.name);
