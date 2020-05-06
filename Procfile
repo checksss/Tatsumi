ed57689b27bb71109bbe1fc5f0217bddf5711757
ItzLightyHD <itzlightyhd@gmail.com>
2020-05-06 22:30:23 +0200

    Remove 2nd instance + Updating packages
    I just removed 2nd instance from the project and I added it as developer instance on Glitch, to make all easier

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: node server.js
-worker2: node server2.js
+worker: node stable
--- server2.js
+++
@@ -1,9 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const shard = new ShardingManager('./tamarro.js', {
-  token: process.env.TOKEN2,
-  autoSpawn: true
-});
-
-shard.spawn(2);
-
-shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/${shard.totalShards}`));
