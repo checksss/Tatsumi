fa33fe5128c14e9ed4ca043e2272b60052587564
ItzLightyHD <itzlightyhd@gmail.com>
2020-05-10 23:33:59 +0200

    Create version.js

---
+++ commands/version.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args, ops) => {
+    message.reply("**UltimateTamarro Source v1.2**\nThis bot was never possible without UltimateTamarro source, and you can host your own instance for free on Heroku.\n__Support Discord server__ » https://discord.gg/HKqb6V7");
+}
