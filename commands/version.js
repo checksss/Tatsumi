6c5be24b027e83196859b87d1f4f81622c7a852e
ItzLightyHD <itzlightyhd@gmail.com>
2020-05-13 14:26:42 +0200

    Wrong!

--- commands/version.js
+++ commands/version.js
@@ -1,3 +1,3 @@
-message.reply("**UltimateTamarro Source v1.2**\nThis bot was never possible without UltimateTamarro source, and you can host your own instance for free on Heroku.\n__Heroku Deploy__ » https://heroku.com/deploy?template=https://github.com/ItzLightyHD/UltimateTamarro\n__Support Discord server__ » https://discord.gg/HKqb6V7");
 exports.run = (client, message, args, ops) => {
+    message.reply("**UltimateTamarro Source v1.2**\nThis bot was never possible without UltimateTamarro source, and you can host your own instance for free on Heroku.\n__Heroku Deploy__ » https://heroku.com/deploy?template=https://github.com/ItzLightyHD/UltimateTamarro\n__Support Discord server__ » https://discord.gg/HKqb6V7");
 }
