23565a6ae7dbf468fb81eec3416f26db1cddf70d
ItzLightyHD <nethernetwork@libero.it>
2020-05-27 10:40:45 +0200

    New commands

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,7 @@ exports.run = (client, message, args, ops) => {
   message.reply("I'm sending you a private message with a list of my commands.");
   let helpMsg = `**List of my commands:**`;
   helpMsg += `\n__${prefix}help__ » Get a private message with a list of commands`;
+  helpMsg += `\n__${prefix}invite__ » Get the invite link for the bot`;
   helpMsg += `\n__${prefix}play__ » Play a song in the current voice channel`;
   helpMsg += `\n__${prefix}leave__ » Makes the bot leave from the current voice channel`;
   helpMsg += `\n__${prefix}pause__ » Pauses the current song`;
@@ -14,7 +15,6 @@ exports.run = (client, message, args, ops) => {
   helpMsg += `\n__${prefix}volume__ » Set the bot volume when playing a song`;
   helpMsg += `\n__${prefix}clear__ (or __${prefix}purge__) » Clear many messages at time`;
   helpMsg += `\n__${prefix}mcstatus__ » Get status of a Minecraft server (Java and MCPE)`;
-  helpMsg += `\n**Tip:** If you add a role named ___mcpestatus__, the default port for the status command will be 19132.`;
   helpMsg += `\n__${prefix}mcskin__ (or __${prefix}skin__) » Get a skin using username (Java only usernames)`;
   helpMsg += `\n__${prefix}ban__ » Helps you to ban a member by using a command`;
   helpMsg += `\n__${prefix}kick__ » Helps you to kick a member by using a command`;
@@ -23,6 +23,6 @@ exports.run = (client, message, args, ops) => {
   helpMsg += `\n__${prefix}unmute__ » Unmute a user in your Discord server`;
   helpMsg += `\n__${prefix}8ball__ » Ask the magic 8ball about your future`;
   helpMsg += `\n__${prefix}meme__ » Get a random meme from meme subreddits`;
-  helpMsg += `\n__${prefix}covid19__ » Get the total cases and deaths of the day for this pandemic situation`;
+  helpMsg += `\n__${prefix}fotd__ » Get a random fact for your day (for realistic experience use it once a day)`;
   message.author.send(helpMsg);
 }
