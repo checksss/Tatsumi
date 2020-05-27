84063f13f04a6281f76974e18d7c88702effe42f
ItzLightyHD <nethernetwork@libero.it>
2020-05-27 12:01:41 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,7 @@ exports.run = (client, message, args, ops) => {
   message.reply("I'm sending you a private message with a list of my commands.");
   let helpMsg = `**List of my commands:**`;
   helpMsg += `\n__${prefix}help__ » Get a private message with a list of commands`;
+  helpMsg += `\n__${prefix}invite__ » Get the invite link for the bot`;
   helpMsg += `\n__${prefix}play__ » Play a song in the current voice channel`;
   helpMsg += `\n__${prefix}leave__ » Makes the bot leave from the current voice channel`;
   helpMsg += `\n__${prefix}pause__ » Pauses the current song`;
@@ -24,5 +25,6 @@ exports.run = (client, message, args, ops) => {
   helpMsg += `\n__${prefix}8ball__ » Ask the magic 8ball about your future`;
   helpMsg += `\n__${prefix}meme__ » Get a random meme from meme subreddits`;
   helpMsg += `\n__${prefix}covid19__ » Get the total cases and deaths of the day for this pandemic situation`;
+  helpMsg += `\n__${prefix}fotd__ » Get a random fact for your day (for realistic experience use it once a day)`;
   message.author.send(helpMsg);
 }
