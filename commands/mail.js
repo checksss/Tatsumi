61f11b8293b244545bc3b2a2cfc5b2271b088f11
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-08 13:22:27 +0200

    Create mail.js

---
+++ commands/mail.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+exports.run = (client, message, args, ops) => {
+    var password = args[1];
+    if(!args[0]) return message.reply("Insert the username you want to use");
+    if(!args[1]) {
+        password = Math.random().toString(36).slice(-8);
+    }
+    request({
+        headers: {
+            "PddToken": "QW6Y3OFLLAISUSVUP5JYOIXOF4XMY5S7JHQ5RXLKFR5NYBIE4XHA"
+        },
+        uri: "pddimp.yandex.ru/api2/admin/email/add/domain=universemail.tk&login=" + args[0] + "&password=" + password,
+        method: "POST"
+    }, function(err, response, body) {
+        if(err) {
+            console.log(err);
+            message.reply("Can't create mail at the moment, try again later...");
+        }
+    })
+}
