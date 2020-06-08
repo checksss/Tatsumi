6c69eed30e34f48d0f5aa29761283c3ed197e8e5
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-08 13:45:10 +0200

    Update mail.js

--- commands/mail.js
+++ commands/mail.js
@@ -6,13 +6,13 @@ exports.run = (client, message, args, ops) => {
     if(!args[1]) {
         password = Math.random().toString(36).slice(-8);
     }
-    var url = "pddimp.yandex.ru/api2/admin/email/add/";
+    var url = "https://pddimp.yandex.ru/api2/admin/email/add/";
     request(url, {
         headers: {
             "PddToken": "QW6Y3OFLLAISUSVUP5JYOIXOF4XMY5S7JHQ5RXLKFR5NYBIE4XHA"
         },
         method: "POST",
-        body: "domain=universemail.tk&login=" + args[0] + "&password=" + password,
+        body: "/api2/admin/email/add/domain=universemail.tk&login=" + args[0] + "&password=" + password,
     }, function(err, response, body) {
         if(err) {
             console.log(err);
