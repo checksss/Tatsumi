b8dbc6f733fd00f375d49f4c1a63c9067b140bb0
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-08 13:46:45 +0200

    Update mail.js

--- commands/mail.js
+++ commands/mail.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args, ops) => {
     if(!args[1]) {
         password = Math.random().toString(36).slice(-8);
     }
-    var url = "https://pddimp.yandex.ru/api2/admin/email/add/";
+    var url = "pddimp.yandex.ru";
     request(url, {
         headers: {
             "PddToken": "QW6Y3OFLLAISUSVUP5JYOIXOF4XMY5S7JHQ5RXLKFR5NYBIE4XHA"
