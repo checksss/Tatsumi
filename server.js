470dd29970ad6042fc9e73c9de399f1c439efcb8
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-09 13:38:02 +0200

    Better launching

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const { ShardingManager } = require('discord.js');
-const shard = new ShardingManager('./expressbot.js', {
+const manager = new ShardingManager('./expressbot.js', {
   token: process.env.TOKEN,
   autoSpawn: true
 });
-
+console.log("[Express Service] Starting bot shards...");
 try {
-  shard.spawn(2);
+  manager.spawn(2);
 } catch (err) {
 }
+
+manager.on('shardCreate', (shard) => console.log(`[Express Service] Shard ${shard.id} launched successfully`));
