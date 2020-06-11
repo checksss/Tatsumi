c35a68d66b07d3ffcb4df52663a804adb23870fb
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-11 14:44:48 +0200

    Update dlmp3.js

--- commands/dlmp3.js
+++ commands/dlmp3.js
@@ -13,14 +13,14 @@ exports.run = async (client, message, args, ops) => {
     let filename = Math.random().toString(36).slice(-8);
 
     let stream = ytdl(args[0], { filter: 'audioonly', format: 'mp3'})
-    .pipe(fs.createWriteStream(`tempdl/${filename}.mp3`), function(err) {
+    .pipe(fs.createWriteStream(`/app/commands/tempdl/${filename}.mp3`), function(err) {
         if (err) {
             console.log(err);
             return message.reply("There was an error executing this command");
         }
     });
 
-    message.channel.send('**' + info.title + '**', {files: [{attachment: `tempdl/${filename}.mp3`, name: filename + '.mp3'}]});
+    message.channel.send('**' + info.title + '**', {files: [{attachment: `/app/commands/tempdl/${filename}.mp3`, name: filename + '.mp3'}]});
     
-    setTimeout(fs.unlinkSync(`tempdl/${filename}.mp3`), 600 * 1000);
+    setTimeout(fs.unlinkSync(`/app/commands/tempdl/${filename}.mp3`), 600 * 1000);
 }
