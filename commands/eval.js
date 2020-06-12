4a2655684d00f48e0e94f7b05abdc02c7598255f
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-12 15:44:12 +0200

    Adding some utils for eval command

--- commands/eval.js
+++ commands/eval.js
@@ -3,6 +3,23 @@ exports.run = async (client, message, args, ops) => {
     if(message.author.id !== ops.ownerID) return message.reply("**Only the bot's creator can use this command!**");
     const { inspect } = require('util');
 
+    function sendEmbed(title, description) {
+        if(!title || !description) return message.reply("Usage: `sendEmbed('My title', 'My description');`");
+        const Discord = require('discord.js');
+        const embed = new Discord.MessageEmbed()
+        .setTitle(title)
+        .setDescription(description)
+        .setColor("RANDOM");
+        message.channel.send(embed);
+    }
+
+    function execute(command, input) {
+        if(!command || !input) return message.reply("Usage: `execute(command, args);`");
+        let commandFile = require(`./${command}.js`);
+        let args = input;
+        commandFile.run(client, message, args, ops);
+    }
+
     try {
         const start = process.hrtime();
         let output = eval(args.join(" "));
