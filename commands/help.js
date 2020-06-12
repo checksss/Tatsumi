c70ddf27e581a13c52b98e296cfe925bfb24444e
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-12 16:34:44 +0200

    Ok

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args, ops) => {
   .setTitle(process.env.BOT_NAME)
   .setDescription("Thank you for using **" + process.env.BOT_NAME + "**, here are all the commands.")
   .addField("🔊 Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`, `dlmp3`")
-  .addField("👮‍♂️ Moderation", "`clear`, `ban`, `kick`, `nick`, `mute`, `unmute`, `serverinfo`")
+  .addField("👮‍♂️ Moderation", "`clear` (or `purge`), `ban`, `kick`, `nick`, `mute`, `unmute`, `serverinfo`")
   .addField("🔥 Fun and Misc", "`help`, `invite`, `mcstatus`, `mcskin` (or `skin`), `8ball`, `meme`, `fotd`, `mcleaks`")
   .addField("🚀 Utility", "`mail`, `covid19`")
   .addField("🔧 Administration (advanced users and bot owner only)", "`eval`, `dev/...`")
