f3a8a7c4db9a8c3be449325adb9988c696c9c59f
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-14 13:41:18 +0200

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -1,22 +1,22 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args, ops) => {
-    let user = message.mentions.members.first();
-    if(!user) return message.reply("You must mention a member");
+    let whois = message.mentions.members.first();
+    if(!whois) return message.reply("You must mention a member");
 
     const whoisEmbed = new Discord.MessageEmbed()
-    .setFooter(user.user.tag, user.user.avatarURL)
+    .setFooter(whois.user.tag, whois.user.avatarURL)
     .setTimestamp()
     .setColor('RANDOM')
     .addField("User information", [
-        `**ID:** ${user.id}`,
-        `**Tag:** ${user.user.tag}`
-        `**Registered on:** ${user.user.createdAt}`,
+        `**ID:** ${whois.id}`,
+        `**Tag:** ${whois.user.tag}`
+        `**Registered on:** ${whois.user.createdAt}`,
     ])
     .addField("Server information", [
-        `**Display name:** ${message.guild.members.find('id', user.id).displayName}`,
-        `**Joined at:** ${message.guild.members.find('id', user.id).joinedAt}`,
-        `**Current roles:** ${message.guild.members.find('id', user.id).roles.join(", ")}`
+        `**Display name:** ${message.guild.members.find('id', whois.id).displayName}`,
+        `**Joined at:** ${message.guild.members.find('id', whois.id).joinedAt}`,
+        `**Current roles:** ${message.guild.members.find('id', whois.id).roles.join(", ")}`
     ]);
     message.channel.send(whoisEmbed);
 }
