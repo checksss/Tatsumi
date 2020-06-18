dc96d519680bcd83f1e7a43caf3808d6e07f621d
ItzLightyHD <itzlightyhd@gmail.com>
2020-06-18 06:34:07 +0000

    Removing "dlmp3" command

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args, ops) => {
   helpMsg = new Discord.MessageEmbed()
   .setTitle(process.env.BOT_NAME)
   .setDescription("Thank you for using **" + process.env.BOT_NAME + "**, here are all the commands.\nIf you have an issue with a command, report it to us using `report bug`")
-  .addField("🔊 Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`, `dlmp3`")
+  .addField("🔊 Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`")
   .addField("👮‍♂️ Moderation", "`clear` (or `purge`), `ban`, `kick`, `nick`, `mute`, `unmute`, `serverinfo`, `whois`")
   .addField("🔥 Fun and Misc", "`help`, `invite`, `mcstatus`, `mcskin` (or `skin`), `8ball`, `meme`, `fotd`, `mcleaks`, `ping`")
   .addField("🚀 Utility", "`mail`, `covid19`, `report`")
