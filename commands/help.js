b62f241ecf8147582a7b45343391c7ca894828f7
ItzLightyHD <nethernetwork@libero.it>
2020-06-29 21:34:39 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args, ops) => {
   .addField("🔊 Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`")
   .addField("👮‍ Moderation", "`clear` (or `purge`), `ban`, `kick`, `nick`, `mute`, `unmute`, `serverinfo`, `whois`, `destroy`")
   .addField("🔥 Fun and Misc", "`help`, `invite`, `mcstatus`, `mcskin` (or `skin`), `8ball`, `meme`, `fotd`, `mcleaks`, `ping`")
-  .addField("🚀 Utility", "`mail`, `covid19`, `report`")
+  .addField("🚀 Utility", "`mail`, `covid19`, `report`, `say`")
   .addField("🔧 Administration (advanced users and bot owner only)", "`eval`, `dev/...`")
   .setColor('RANDOM')
   message.channel.send(helpMsg);
