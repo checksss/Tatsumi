55d0188990241fd3a65f30475d1b65197f21aa53
ItzLightyHD <nethernetwork@libero.it>
2020-06-29 21:45:28 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args, ops) => {
     const toSay = args.slice(0).join(' ');
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have the permission to execute this command!");
     if(!toSay) return message.reply("What you want to make me say?");
     message.channel.messages.fetch({ limit: 1 }).then(messages => {
             message.channel.bulkDelete(messages)
