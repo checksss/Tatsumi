e470164698b4d3b8b22ee027d9c8f84baa748348
ItzLightyHD <nethernetwork@libero.it>
2020-06-29 21:34:20 +0200

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,8 @@
+exports.run = (client, message, args, ops) => {
+    const toSay = args.slice(0).join(' ');
+    if(!toSay) return message.reply("What you want to make me say?");
+    message.channel.messages.fetch({ limit: 1 }).then(messages => {
+            message.channel.bulkDelete(messages)
+    });
+    message.channel.send(toSay);
+}
