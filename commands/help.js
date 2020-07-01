fe775bd622fa0a7f8d3e5120d82dbd8662033d44
checksss <67140193+checksss@users.noreply.github.com>
2020-06-30 23:20:57 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args, ops) => {
   .setDescription("Thank you for using **" + process.env.BOT_NAME + "**, here are all the commands.\nIf you have an issue with a command, report it to us using `report bug`")
   .addField("🔊 Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`")
   .addField("👮‍ Moderation", "`clear` (or `purge`), `ban`, `kick`, `nick`, `mute`, `unmute`, `serverinfo`, `whois`, `destroy`")
-  .addField("🔥 Fun and Misc", "`help`, `invite`, `mcstatus`, `mcskin` (or `skin`), `8ball`, `meme`, `fotd`, `mcleaks`, `ping`")
+  .addField("🔥 Fun and Misc", "`help`, `invite`, `8ball`, `meme`, `fotd`, `ping`")
   .addField("🚀 Utility", "`mail`, `covid19`, `report`, `say`")
   .addField("🔧 Administration (advanced users and bot owner only)", "`eval`, `dev/...`")
   .setColor('RANDOM')
