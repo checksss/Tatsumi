836e43a4fb4116b3b5fcaddbfd0ab9e73e9f72f9
checksss <67140193+checksss@users.noreply.github.com>
2020-06-30 23:47:11 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,12 +5,12 @@ exports.run = (client, message, args, ops) => {
   helpMsg = new Discord.MessageEmbed()
   .setTitle(process.env.BOT_NAME)
   .setDescription("Thank you for using **" + process.env.BOT_NAME + "**, here are all the commands.\nIf you have an issue with a command, report it to us using `report bug`")
-  .addField("🔊 Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`")
+  .addField(":loud_sound: Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`")
   .addField("👮‍ Moderation", "`clear` (or `purge`), `ban`, `kick`, `nick`, `mute`, `unmute`, `serverinfo`, `whois`, `destroy`")
-  .addField("🔥 Fun and Misc", "`help`, `invite`, `8ball`, `meme`, `fotd`, `ping`")
-  .addField("🚀 Utility", "`mail`, `covid19`, `report`, `say`")
-  .addField("🔧 Administration (advanced users and bot owner only)", "`eval`, `dev/...`")
-  .addField("made by checks#4145")", "checks is my creator")
+  .addField(":fire: Fun and Misc", "`help`, `invite`, `8ball`, `meme`, `fotd`, `ping`")
+  .addField(":rocket: Utility", "`mail`, `covid19`, `report`, `say`")
+  .addField(":wrench: Administration (advanced users and bot owner only)", "`eval`, `dev/...`")
+  .addField("made by checks#4145")",  "`checks is my creator.`")
   .setColor('RANDOM')
   message.channel.send(helpMsg);
 }
